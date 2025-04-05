d7a8489e37b17a67694d93cf75ed2543d01e6233
Adnan Ayaz <20pwcse1860@uetpeshawar.edu.pk>
2025-04-05 20:03:34 +0500

    c

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,33 +1,5 @@
-import { IAnswer } from "@/models/answer";
 import { Answer, User, Question } from "@prisma/client";
 
-export interface QuestionType {
-  _id: string;
-  title: string;
-  content: string;
-  tags: string[];
-  author: UserType;
-  upVotes: number;
-  answers: IAnswer[];
-  views: number;
-  createdAt: Date;
-}
-
-export interface UserType {
-  _id: string;
-  name: string;
-  email: string;
-  bio?: string;
-  image?: string;
-  location?: string;
-  portfolio?: string;
-  reputation?: number;
-  provider: string; // Store the provider (e.g., 'github', 'google', 'credentials')
-  providerAccountId: string; // Store the unique provider account ID
-  password?: string; // For credentials-based login, if password is needed
-  bookmarks?: string[];
-  createdAt: Date;
-}
 
 export interface ProcessedSearchParams {
   searchQuery: string;
